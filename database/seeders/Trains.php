350dfb29f2b48b3ff9ab0ac29c4647805a03f8e5
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-10 15:49:44 +0200

    utilizzato faker per le colonne

--- database/seeders/Trains.php
+++ database/seeders/Trains.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class Trains extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $newTrain = new Train();
+        $newTrain -> company= $faker->words();
+        $newTrain -> departure_station= $faker->words(3, true);
+        $newTrain -> arrival_station= $faker->words(3, true);
+        $newTrain -> departure_time= $faker->time();
+        $newTrain -> arrival_time= $faker->time();
+        $newTrain -> train_code= $faker ->lexify();
+        $newTrain -> on_time= $faker->boolean();
+        $newTrain -> canceled= $faker->boolean();
+
+
+
     }
 }
