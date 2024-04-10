4d9a52684ea34c8a51813432bed7c1e9b59656e6
arianna-cosimini <arianna.cosimini@gmail.com>
2024-04-10 15:17:26 +0200

    Create colonne della tabella trains

--- database/migrations/2024_04_10_130214_create_trains_table.php
+++ database/migrations/2024_04_10_130214_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code');
+            $table->integer('carriages_number');
+            $table->boolean('on_time')->default(false);
+            $table->boolean('canceled')->default(true);
             $table->timestamps();
         });
     }
